9496e619f724e21fda23577038c19ddb6430b39f
jazoomi <sadiqazmi0@gmail.com>
2022-11-30 23:27:40 -0500

    Finished the main logic
    need add node

--- matching_game.js
+++ matching_game.js
@@ -1,10 +1,16 @@
-var amount = 0;
-var shapes = {};
-var shapeCheck = 0;
+var amount = 0; // this is the length of the shapes ( the round)
+var shapes = {}; //this is the shape order, 1 isrectangle, 2 is circle, 3 square, 4 triangle
+var shapeCheck = 0; // this will keep track what shape number to check when a shape is pressed in attempt to match
+var names = {}; //this is an array that stores the names
+var players = 0; // this is the amount of people on the leaderboard
 function Game(){
+    amount = 0;
+    shapes = {};
+    shapeCheck = 0;
     generateShape();
     iteration();
-
+    names [players] = document.getElementById("fname").value;
+    document.getElementById("fname").disabled = true;
     document.getElementById("start-game").disabled = true;
 
 }
@@ -40,34 +46,41 @@ function triangle(n4){
 
 //interates through all the shapes up until now
 function iteration(){
-    document.getElementById("lose").innerHTML = shapes[amount] + "" +amount;
-
-    for (var i = 0; i < amount; i++){
+    var i = 0;
+    myInterval = setInterval(function(){ 
 
         if ( shapes[i] == 1){
-            setTimeout(function(){document.getElementById("rectangle").style.backgroundColor = "red"}, 1000);
-            setTimeout(function(){document.getElementById("rectangle").style.backgroundColor = "#555"}, 2000);
+            document.getElementById("rectangle").style.backgroundColor = "red"
+            setTimeout(function(){document.getElementById("rectangle").style.backgroundColor = "#555"}, 1000);
         }
         else if (shapes[i] == 2){
-            setTimeout(function(){document.getElementById("circle").style.backgroundColor = "red"}, 1000);
-            setTimeout(function(){document.getElementById("circle").style.backgroundColor = "#555"}, 2000);
+          document.getElementById("circle").style.backgroundColor = "red"
+            setTimeout(function(){document.getElementById("circle").style.backgroundColor = "#555"}, 1000);
         }
         else if (shapes[i] == 3){
-            setTimeout(function(){document.getElementById("square").style.backgroundColor = "red"}, 1000);
-            setTimeout(function(){document.getElementById("square").style.backgroundColor = "#555"}, 2000);
+            document.getElementById("square").style.backgroundColor = "red"
+            setTimeout(function(){document.getElementById("square").style.backgroundColor = "#555"}, 1000);
         }
         else {
-            setTimeout(function(){document.getElementById("triangle").style.borderBottomColor = "red"}, 1000);
-            setTimeout(function(){document.getElementById("triangle").style.borderBottomColor = "#555"}, 2000);
+            document.getElementById("triangle").style.borderBottomColor = "red"
+            setTimeout(function(){document.getElementById("triangle").style.borderBottomColor = "#555"}, 1000);
         }
+        i++;
+        document.getElementById("lose").innerHTML = i + " Round: " + amount;
 
-    }
+        if (i >= amount){
+            clearInterval(myInterval);
+        }
+    }, 2000);
+
+    
     
 }
 //add a new shape only to the end of the array
 function generateShape() {
     
     var shape = Math.floor(Math.random()*4 + 1);
+    var next = shapes.length;
     if ( shape == 1){
         shapes[amount] = shape;
         amount++;
@@ -98,5 +111,9 @@ function checkShape(n){
     else{
         document.getElementById("lose").innerHTML = "Game over! you lose!";
         shapeCheck = -1;
+        document.getElementById("start-game").disabled = false; //turn the button back on
+        document.getElementById("fname").disabled = false; //turn the textbox back on
+        document.getElementById("scores").innerHTML +=  names[players] + " " + amount;
+        players++;
     }
 }
